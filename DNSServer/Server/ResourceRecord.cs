645924fe9e691e757f82872f8026b57493dfc69a
playstation4 <teppopp@mail.ru>
2016-04-24 14:50:09 +0500

    Start

---
+++ DNSServer/Server/ResourceRecord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNSServer.Server
+{
+    public enum ResourceRecordType
+    {
+        A = 1,
+        NS = 2,
+        CNAME = 5,
+        SOA = 6,
+        PTR = 12,
+        MX = 15
+    }
+
+    class ResourceRecord
+    {
+        public string Name { get; set; }
+        public ResourceRecordType Type { get; set; }
+
+    }
+}
